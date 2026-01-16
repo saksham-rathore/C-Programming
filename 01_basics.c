07f5db769111671801a45f827ed67996b67c361e
saksham762007 <samrathore07062007@gmail.com>
2026-01-16 19:23:19 +0530

    file update

---
+++ 01_basics.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Main function: program execution starts here
+int main(void) {
+    
+    // Your code starts here
+    printf("Hello, World!\n");
+    
+    return 0; // Indicates successful execution
+}
