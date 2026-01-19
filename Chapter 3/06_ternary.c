d51061ad129f82dd1e8f800165f2a0292995ab74
saksham762007 <samrathore07062007@gmail.com>
2026-01-19 20:17:58 +0530

    file update

--- Chapter 3/06_ternary.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    // condition ? expression-if-true : expression-if-false
-    int a = 345;
-    int b= 345452;
-    a>b?printf("a is greater"):printf("b is greater");
-}
