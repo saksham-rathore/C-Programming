2c8a5286bf8efda4fe59bae64d02d2f1763ffb37
saksham762007 <samrathore07062007@gmail.com>
2026-01-23 20:52:05 +0530

    file update

--- Chapter 8 - Practice Set/Practice_program/1.c
+++ Chapter 8 - Practice Set/Practice_program/1.c
@@ -1,15 +1,28 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
+// int main() {
+//     int num;
+    
+//     printf("Enter the value of a number whose multiplication table is to be printed\n");
+//     scanf("%d", &num);
+//     printf("the multiplication table of %d is \n", num);
+    
+//     for (int i = 0; i <= 10; i++)
+//     {
+//         printf("%d * %d = %d\n",num, i, i*num);
+//     }   
+//     return 0; 
+// }
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
     int num;
+    printf("%d + %d = %d", num)
     
-    printf("Enter the value of a number whose multiplication table is to be printed\n");
-    scanf("%d", &num);
-    printf("the multiplication table of %d is \n", num);
-    
-    for (int i = 0; i <= 10; i++)
-    {
-        printf("%d * %d = %d\n",num, i, i*num);
-    }   
-    return 0; 
+    return 0;
 }
