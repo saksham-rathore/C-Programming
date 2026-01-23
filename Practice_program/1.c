63900fd6b33120af3a75619ec563cd6ed5aaf6c9
saksham762007 <samrathore07062007@gmail.com>
2026-01-23 21:13:15 +0530

    file update

---
+++ Practice_program/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter the value of a number whose multiplication table is to be printed\n");
+    scanf("%d", &num);
+    printf("the multiplication table of %d is \n", num);
+    
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d * %d = %d\n",num, i, i*num);
+    }   
+    return 0; 
+}
